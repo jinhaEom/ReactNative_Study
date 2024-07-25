325d66a9de8c7246f286708c5c0288973ce9788c
jinhaeom <umjin98@naver.com>
2024-07-25 17:27:13 +0900

    navigation 이동 study

---
+++ food-category-rn-app/screens/MealsOverviewScreen.js
@@ -0,0 +1,19 @@
+import { MEALS } from "../data/dummy-data";
+import { Text, View, StyleSheet } from "react-native";
+
+function MealsOverviewScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Meals OverView Screen</Text>
+        </View>
+    );
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
