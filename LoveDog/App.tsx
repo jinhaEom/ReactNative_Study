86931b0af77362665ac81d1e1433fcc48fba72b2
jinhaeom <umjin98@naver.com>
2025-01-10 17:27:44 +0900

    firebase Storage, Realtime DB 연결 및 프로플 사진등록 Study

--- LoveDog/App.tsx
+++ LoveDog/App.tsx
@@ -3,10 +3,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { RootStackNavigation } from './src/navigation/RootStackNavigation';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { WEB_CLIENT_ID } from './src/constants/Constants';
 
 // Google 로그인 초기화
 GoogleSignin.configure({
-  webClientId: '705409136322-dec5ojmbp57oba0niui4u4gqo069k4i4.apps.googleusercontent.com',
+  webClientId: WEB_CLIENT_ID,
 });
 
 export default function App() {
